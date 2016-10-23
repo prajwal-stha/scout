aef7c75df17228d74882a71b70f0c80c5f6eab83
Prajwal Shrestha <prajwal_stha@yahoo.com>
2016-10-23 11:49:43 +0545

    Add minor fixes

--- resources/views/admin/formrequest.blade.php
+++ resources/views/admin/formrequest.blade.php
@@ -32,7 +32,7 @@
                                     <td><a data-toggle="tooltip" title="VIEW UNIT" class="team-name"
                                            href="{{ url('admin/view-organization', [$value->id]) }}">{{ $value->name }}</a>
                                     </td>
-                                    <td><a data-toggle="tooltip" title="VIEW USER" href="{{ url('admin/profile', [$value->user_id]) }}"></a></td>
+                                    <td><a data-toggle="tooltip" title="VIEW USER" href="{{ url('admin/profile', [$value->user_id]) }}">{{ $value->user->f_name }} {{ $value->user->l_name }}</a></td>
                                     <td>
                                         <a data-toggle="tooltip" title="VIEW" class="btn btn-primary" href="{{ url('admin/view-organization', [$value->id]) }}"><i
                                                     class="fa fa-eye"></i></a>
